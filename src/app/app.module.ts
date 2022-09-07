75f81563d4c778f0485eb46797e3bfe22398bf10
Bruno Omizu <y.omizu@gmail.com>
2022-09-07 17:41:35 +1200

    main page + experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { TimelineModule } from 'primeng/timeline';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
+    ExperienceComponent,
+    EducationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ButtonModule,
+    TimelineModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/experience/experience.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { PrimeIcons } from 'primeng/api';
+
+@Component({
+  selector: 'app-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.scss'],
+})
+export class ExperienceComponent implements OnInit {
+  events: any[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.events = [
+      {
+        status: 'Mercury NZ',
+        date: 'August 2021 - Present',
+        icon: PrimeIcons.CIRCLE,
+        color: '#9C27B0',
+        description:
+          'Responsible for technology thought leadership, providing the elaboration and detailed design of Mercury’s technology solutions and the oversight of solution implementations relating to the Digital platforms and associated ecosystem.',
+      },
+      {
+        status: 'Vivo (Telefonica Brazil)',
+        location: 'Sao Paulo (Brazil)',
+        date: 'April 2020 - May 2021',
+        icon: PrimeIcons.CIRCLE,
+        color: '#673AB7',
+        description:
+          'Defined technology roadmap for Cloud (Azure, AWS and GCP) and DevOps projects',
+      },
+      {
+        status: 'Accenture New Zealand',
+        date: 'July 2016 - March 2020',
+        icon: PrimeIcons.CIRCLE,
+        color: '#FF9800',
+        description:
+          'Defined the technology strategy and roadmap to automate code development while building tasks using DevOps tools and principles',
+      },
+      {
+        status: 'Accenture Brazil',
+        date: 'March 2007 - July 2016',
+        icon: PrimeIcons.CIRCLE,
+        color: '#607D8B',
+        description:
+          'Improved system quality by identifying issues and common patterns, and developing standard operating procedures',
+      },
+    ];
+  }
+}
