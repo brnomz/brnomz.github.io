5aa0a5183a5eb88fac18dc6b6dfa2e5890f674da
Bruno Omizu <y.omizu@gmail.com>
2022-09-09 16:51:19 +1200

    added skills section + refactoring other components

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,41 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-  ngOnInit (): void {}
+  skills: any[] = []
+  stars: number[] = []
+
+  ngOnInit (): void {
+    this.skills = [
+      {
+        name: 'Back-End',
+        description: 'AWS Lambda, Azure Functions, Node.js, Python, Typescript',
+        stars: 3
+      },
+      {
+        name: 'Cloud',
+        description: 'AWS, Azure',
+        stars: 4
+      },
+      {
+        name: 'DevOps',
+        description:
+          'Azure DevOps, Bitbucket, Bamboo, CircleCI, GitLab, GitHub',
+        stars: 5
+      },
+      {
+        name: 'Front-End',
+        description: 'Angular, CSS, Cypress, HTML, TypeScript',
+        stars: 3
+      },
+      {
+        name: 'Project Management',
+        description: 'Confluence, JIRA, LucidCharts, Miro',
+        stars: 3
+      }
+    ]
+  }
+
+  starsArray (length: number): number[] {
+    return (this.stars = new Array(length))
+  }
 }
