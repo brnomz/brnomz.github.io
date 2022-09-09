85e0022877e198c14ff2bfa8fd741b92cc7c8fb9
Bruno Omizu <y.omizu@gmail.com>
2022-09-09 23:57:19 +1200

    fix flex header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  ngOnInit (): void {}
 
-  ngOnInit(): void {}
-
-  gotoTop() {
+  gotoTop (): void {
     window.scroll({
       top: 0,
       left: 0,
-      behavior: 'smooth',
-    });
-  }
-
-  scroll(id: string) {
-    console.log(`scrolling to ${id}`);
-    let element = document.getElementById(id);
-    element?.scrollIntoView(true);
+      behavior: 'smooth'
+    })
   }
 }
