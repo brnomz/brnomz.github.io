bbfdbf2c3c3e3322d9035cc728776b73eb854ed4
Bruno Omizu <y.omizu@gmail.com>
2022-09-09 12:28:09 +1200

    move header styling to styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
-import { TimelineModule } from 'primeng/timeline';
-import { ExperienceComponent } from './experience/experience.component';
-import { EducationComponent } from './education/education.component';
-import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component'
+import { MainPageComponent } from './main-page/main-page.component'
+import { CardModule } from 'primeng/card'
+import { ButtonModule } from 'primeng/button'
+import { TimelineModule } from 'primeng/timeline'
+import { ExperienceComponent } from './experience/experience.component'
+import { EducationComponent } from './education/education.component'
+import { NgxSpinnerModule } from 'ngx-spinner'
+import { SkillsComponent } from './skills/skills.component'
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     MainPageComponent,
     ExperienceComponent,
     EducationComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
@@ -29,10 +31,10 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     CardModule,
     ButtonModule,
     TimelineModule,
-    NgxSpinnerModule.forRoot({ type: 'square-jelly-box' }),
+    NgxSpinnerModule.forRoot({ type: 'square-jelly-box' })
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
